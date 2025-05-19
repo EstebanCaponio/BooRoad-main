afa33f588582033b3f837649e08d396658aee086
EstebanCaponio <estebancaponio@gmail.com>
2025-05-19 11:32:24 +0200

    feat: booroad2.0

---
+++ src/pages/DetailTrip.jsx
@@ -0,0 +1,46 @@
+import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import TripContext from "../context/TripContext";
+
+function TripDetails() {
+  const { id } = useParams();
+  const { ArrayTrip } = useContext(TripContext);
+
+  return (
+    <div className="container my-5">
+      {ArrayTrip.map((trip) =>
+        trip.id.toString() === id ? (
+          <div key={trip.id} className="card shadow">
+            <div className="row g-0">
+              <div className="col-md-6">
+                <img
+                  src={`/public/${trip.image}`}
+                  alt={trip.destination}
+                  className="img-fluid rounded-start h-100 object-fit-cover"
+                />
+              </div>
+              <div className="col-md-6">
+                <div className="card-body">
+                  <h2 className="card-title">{trip.destination}</h2>
+                  <p className="card-text">
+                    <strong>Operatore:</strong> {trip.operator}
+                  </p>
+                  <p className="card-text">
+                    <strong>Stato:</strong> {trip.status}
+                  </p>
+                  <p className="card-text">
+                    <strong>Partenza:</strong> {trip.start_date}
+                  </p>
+                  <p className="card-text">
+                    <strong>Ritorno:</strong> {trip.end_date}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : null
+      )}
+    </div>
+  );
+}
+export default TripDetails;
---
+++ src/pages/DetailUser.jsx
@@ -0,0 +1,53 @@
+import { useContext } from "react";
+import TripContext from "../context/TripContext";
+import { useParams } from "react-router-dom";
+
+function DetailUser() {
+  const { id } = useParams();
+  const { ArrayTrip } = useContext(TripContext);
+
+  // console.log(currentTrip)
+  const currentTrip = ArrayTrip[id - 1];
+  console.log(currentTrip)
+  const partecipants = currentTrip.partecipants;
+
+
+  return (
+    <>
+      <div className="container my-4">
+        <form className="row g-3 mb-3">
+          <div className="col-md-8">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Cerca partecipante"
+            />
+          </div>
+          <div className="col-md-4">
+            <button type="submit" className="btn btn-primary w-100">
+              Cerca
+            </button>
+          </div>
+        </form>
+        {partecipants.length > 0 ? (
+          partecipants.map((e) => (
+            <div key={e.user_id} className="card text-center">
+              <div className="card-header">{`${e.surname} ${e.name}`}</div>
+              <div className="card-body">
+                <p className="card-text">Email: {e.email}</p>
+                <p className="card-text">Numero di telefono: {e.phone_number}</p>
+                <p className="card-text">Codice Fiscale: {e.CF}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>
+            <h1>Nessun Viaggio disponibile</h1>
+          </div>
+        )}
+      </div >
+    </>
+  );
+}
+
+export default DetailUser;
